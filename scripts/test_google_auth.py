471ffdbfd131188d75d24fb675b429e3c1d5ebf0
Nikhil <reyrinbaf@gmail.com>
2025-06-07 14:20:21 +0530

    modified and test for g-auth

--- scripts/test_google_auth.py
+++ scripts/test_google_auth.py
@@ -1,13 +1,22 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
 import os
+from google_auth_oauthlib.flow import InstalledAppFlow
 from dotenv import load_dotenv
 
 load_dotenv()
 
-scopes = os.getenv("GOOGLE_API_SCOPES").split(",")
-creds_path = os.getenv("GOOGLE_CREDENTIALS_PATH")
+client_config = {
+    "installed": {
+        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+        "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "redirect_uris": ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"]
+    }
+}
 
-flow = InstalledAppFlow.from_client_secrets_file(creds_path, scopes)
+scopes = os.getenv("GOOGLE_API_SCOPES").split(",")
+
+flow = InstalledAppFlow.from_client_config(client_config, scopes)
 creds = flow.run_local_server(port=0)
 
 print("Access Token:", creds.token)
